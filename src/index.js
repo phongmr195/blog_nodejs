f32c1cbf7f6a5cd3ba11b12d8101767caffdd7b4
phongmr195 <vdphong1995@gmail.com>
2022-06-09 23:31:47 +0700

    Config source

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const path = require('path');
 const express = require('express');
-const moran = require('morgan');
+const morgan = require('morgan');
 const handlebars = require('express-handlebars');
 const app = express();
 const port = 5000;
 
-app.use(moran('combined'));
-app.engine('handlebars', handlebars.engine());
-app.set('view engine', 'handlebars');
+// Load static foler
+app.use(express.static(path.join(__dirname, 'public')));
+// Use morgan
+app.use(morgan('combined'));
+// Use tempalte engine
+app.engine('hbs', handlebars.engine({
+    extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, '/resources/views'));
 console.log(path.join(__dirname, '/resources/views'));
 
 
-app.get('/', (req, res) => res.send('Xin chao cac ban!'));
+app.get('/', (req, res) => {
+    return res.render('home');
+});
 app.listen(port, () => console.log(`Test example app listening at http://localhost:${port}`));
